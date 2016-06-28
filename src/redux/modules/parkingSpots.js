ed571d1282387a121d1d288accf5e6a569bd59ea
Dave Edelhart <dave@wonderlandlabs.com>
2016-06-27 23:45:08 -0700

    got search form working

--- src/redux/modules/parkingSpots.js
+++ src/redux/modules/parkingSpots.js
@@ -34,7 +34,28 @@ const initialState = [
         x: 50,
         y: 60,
         id: 100,
-        cost: '0.25/min',
+        cost: '0.25/10 min',
+        spots: 2
+    },
+    {
+        x: 100,
+        y: 200,
+        id: 200,
+        cost: '15.00/all day',
+        spots: 2
+    },
+    {
+        x: 300,
+        y: 150,
+        id: 300,
+        cost: 'free',
+        spots: 2
+    },
+    {
+        x: 400,
+        y: 300,
+        id: 400,
+        cost: '0.10/10 min',
         spots: 2
     }
 ];
--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -2,12 +2,33 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 // import {increment, doubleAsync} from '../../redux/modules/parkingSpots';
-import {display} from '../../redux/modules/reservation';
+import {display, send} from '../../redux/modules/reservation';
 import ParkingSpot from '../../containers/ParkingSpot';
-
 import map from './maps/streets.png';
+import InputRange from 'react-input-range';
+
 import toolbar from './toolbar.png';
-const mapStyle = {background: `url(${map})`};
+import location from './icons/location.png';
+import calendar from './icons/calendar.png';
+import time from './icons/clock.png';
+import you from './icons/you.svg';
+
+const bg = img => ({backgroundImage: `url(${img})`});
+
+const mapStyle = bg(map);
+const locIconStyle = bg(location);
+const dateStyle = bg(calendar);
+const timeStyle = bg(time);
+const compactStyle = {
+    height: 'auto',
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    color: 'inherit'
+};
+
+const compactStyleNM = Object.assign({margin: 0}, compactStyle);
 
 import classes from './HomeView.scss';
 
@@ -18,36 +39,97 @@ import classes from './HomeView.scss';
 // NOTE: You can run `npm run flow:check` to check for any errors in your
 // code, or `npm i -g flow-bin` to have access to the binary globally.
 // Sorry Windows users :(.
-type
-Props = {
+type Props = {
     parkingSpots: array,
     reservation: object,
-    display: Function
+    display: any
 };
 
 // We avoid using the `@connect` decorator on the class definition so
 // that we can export the undecorated component for testing.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 export class HomeView extends React.Component<void, Props, void> {
+    constructor (props) {
+        super(props);
+        this.state = {newReservation: {reserveFor: 60}};
+    }
+
     static propTypes = {
         parkingSpots: PropTypes.array,
         reservation: PropTypes.object,
-        display: PropTypes.function
+        display: PropTypes.func,
+        send: PropTypes.func
     };
 
     render () {
         console.log('parking spots: ', this.props.parkingSpots);
-        const spots = this.props.parkingSpots.map((spot) => <ParkingSpot key={`parkingspot${spot.id}`} spot={spot}/>);
+        let spots = '';
         const displayInput = function () {
-            debugger;
             this.props.display(true);
         }.bind(this);
 
         let resform = '';
 
+        if (this.props.reservation.sent) {
+            spots = this.props.parkingSpots.map((spot) => <ParkingSpot key={`parkingspot${spot.id}`} spot={spot}/>);
+        }
+
         if (this.props.reservation.display) {
+            const changeRes = function (component, values) {
+                var newReservation = Object.assign({}, this.state.newReservation, {reserveFor: values});
+                this.setState({newReservation});
+            }.bind(this);
+
+            const search = function () {
+                this.props.display(false);
+                this.props.send();
+            }.bind(this);
+
+            const changeDate = function (evt) {
+                this.setState({newReservation: Object.assign({}, this.state.newReservation, {date: evt.value})});
+            }.bind(this);
+            const changeTime = function (evt) {
+                this.setState({newReservation: Object.assign({}, this.state.newReservation, {time: evt.value})});
+            }.bind(this);
+
             resform = <div className={classes.resform}>
-                <div className={classes.resform__inner}>Reservation</div>
+                <div className={classes.resform__inner}>
+                    <div className={classes.resform__formrow}>
+                        <div className={classes.resform__icon} style={locIconStyle}>
+                        </div>
+                        <div className={classes.resform__inputCell}><input type="text" name="location"
+                                                                           placeholder="Location"/>
+                        </div>
+                    </div>
+
+                    <div className={classes.resform__formrow}>
+                        <div className={classes.resform__icon} style={dateStyle}>
+                        </div>
+                        <div className={classes.resform__inputCell}><input type="text" value={this.state.newReservation.date} name="date" onChange={changeDate} placeholder="Date"/>
+                        </div>
+                        <div className={classes.resform__icon} style={timeStyle}>
+                        </div>
+                        <div className={classes.resform__inputCell}><input type="text" name="time" value={this.state.newReservation.time} onChange={changeTime} placeholder="Time"/>
+                        </div>
+                    </div>
+
+                    <div className={classes.resform__formrow} style={compactStyle}>
+                        <div className={classes.resform__label}>
+                            Reserve For:
+                        </div>
+                    </div>
+                    <div className={classes.resform__formrow} style={compactStyleNM}>
+                        <div className={classes.resform__reserveForCell}>
+                            <InputRange minValue={15} value={this.state.newReservation.reserveFor} maxValue={120}
+                                        step={15} onChange={changeRes}/>
+                        </div>
+                    </div>
+                    <div className={classes.resform__formrow}>
+                        <center>
+                            <button onClick={search} className={classes.resform__submitBtn}>Search</button>
+                        </center>
+                    </div>
+                </div>
             </div>;
         }
 
@@ -56,6 +138,9 @@ export class HomeView extends React.Component<void, Props, void> {
                 <h1 className={classes.appframe__title}>Parking App</h1>
                 <section className={classes.appframe__map} style={mapStyle}>
                     {spots}
+                    <div className={classes.you}>
+                        <img src={you} />
+                    </div>
                     {resform}
                 </section>
                 <nav className={classes.appframe__nav}><img src={toolbar} onClick={displayInput}/></nav>
@@ -69,5 +154,5 @@ const mapStateToProps = (state) => ({
     reservation: state.reservation
 });
 export default connect((mapStateToProps), {
-    display
+    display, send
 })(HomeView);
